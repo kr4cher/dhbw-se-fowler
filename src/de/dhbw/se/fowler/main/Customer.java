7a8d81d23e1ae439221a21c45c793e6f50b7269d
Lukas Karcher <32646371+kr4cher@users.noreply.github.com>
2019-04-30 16:28:43 +0200

    Extract Method

--- src/de/dhbw/se/fowler/main/Customer.java
+++ src/de/dhbw/se/fowler/main/Customer.java
@@ -38,14 +38,18 @@ public class Customer {
 			if (isTwoDayRelease(rental))
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result += formatResult(thisAmount, rental);
 			totalAmount += thisAmount;
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
+	}
+
+	private String formatResult(double thisAmount, Rental rental) {
+		return "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
+				+ String.valueOf(thisAmount) + "\n";
 	}
 
 	private boolean isTwoDayRelease(Rental rental) {
