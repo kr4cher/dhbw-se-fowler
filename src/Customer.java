d7d5877f72255b43f5df872387ab5c654fd571ce
Lukas Karcher <32646371+kr4cher@users.noreply.github.com>
2019-04-30 15:19:19 +0200

    Rename Variable

--- src/Customer.java
+++ src/Customer.java
@@ -7,12 +7,12 @@ class Customer {
 	private String name;
 	private Vector rentals = new Vector();
 
-	public Customer(String newname) {
-		name = newname;
+	public Customer(String name) {
+		this.name = name;
 	};
 
-	public void addRental(Rental arg) {
-		rentals.addElement(arg);
+	public void addRental(Rental rental) {
+		rentals.addElement(rental);
 	};
 
 	public String getName() {
