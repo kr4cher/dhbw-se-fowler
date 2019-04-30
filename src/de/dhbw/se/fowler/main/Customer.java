69d08b0c6a6145749dc2096c062e052d895535eb
Lukas Karcher <32646371+kr4cher@users.noreply.github.com>
2019-04-30 16:01:54 +0200

    Replace Nested Conditional with Guard Clauses

--- src/de/dhbw/se/fowler/main/Customer.java
+++ src/de/dhbw/se/fowler/main/Customer.java
@@ -49,23 +49,15 @@ public class Customer {
 	}
 
 	private double amountFor(Rental rental) {
-		double thisAmount = 0;
 		switch (rental.getMovie().getPriceCode()) {
 		case Movie.REGULAR:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
-			break;
+			return rental.getDaysRented() > 2 ? (rental.getDaysRented() - 2) * 1.5 + 2 : 2;
 		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
+			return rental.getDaysRented() * 3;
 		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
-			break;
+			return rental.getDaysRented() > 3 ? (rental.getDaysRented() - 3) * 1.5 + 1.5 : 1.5;
 		}
-		return thisAmount;
+		return 0;
 	}
 
 }
